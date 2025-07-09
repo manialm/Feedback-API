75056a3099a308db3a69d432aa0e7e71f28912ed
Mani Alimadadi <mani.alimadadi7@gmail.com>
2025-07-09 21:35:53 +0330

    Add user model

---
+++ user/model.py
@@ -0,0 +1,12 @@
+from sqlmodel import SQLModel, Field
+
+class UserBase(SQLModel):
+    username: str = Field(index=True)
+
+class User(UserBase, table=True):
+    # TODO: | None?
+    id: int | None = Field(default=None, primary_key=True)
+    password_hash: bytes | None = Field(default=None)
+
+class UserCreate(UserBase):
+    password: bytes
